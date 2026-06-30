fb64f7974766759f7d97b3835cf052eec3d6d8c6
LL-Wedjat <LL-Wedjat@users.noreply.github.com>
2026-06-30 16:02:26 +0000

    deploy: build from 6cc80b89ee33ee667892dc75c74982b2c2c0a5d5

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -33,8 +33,8 @@ export default {
   assets: {
     'index.csr.html': {size: 9544, hash: '836895864d0f7d7d0c816eb3e6b9358d2b18526ded6d2232ebca0f25b1a84f96', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
     'index.server.html': {size: 1246, hash: 'f9d0099c47f4df8304af02912ef1bce8902daf6cf25e0ed6753f987036f76170', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'liquid-light/index.html': {size: 82127, hash: '2cc392640eb7cc6fd93fff367fb7b4963c13b08f7e1f315dbe842675475aa912', text: () => import('./assets-chunks/liquid-light_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 50413, hash: '5ea986814e52c40a72133ff5e0bae18cb3171f1c87370b6b74fbcdd0da449a6e', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.html': {size: 50595, hash: '91da3f6828f78905e91d13f0b922c19ae6eeb8b5aa1c702ad9be49d9d5fc746d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'liquid-light/index.html': {size: 82218, hash: '87dca0fdbaffece294bf0106d5bbba446964adb1c4233644ef1c3a863973cdc4', text: () => import('./assets-chunks/liquid-light_index_html.mjs').then(m => m.default)},
     'styles-QQQNFOI3.css': {size: 40870, hash: 'RBmf199WvCI', text: () => import('./assets-chunks/styles-QQQNFOI3_css.mjs').then(m => m.default)}
   },
 };
